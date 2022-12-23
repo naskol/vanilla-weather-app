cd57f5b8c835c04aebba2fdced468eea9dfe7239
naskol <44285828+naskol@users.noreply.github.com>
2022-12-23 18:17:19 +0200

    Add Search Engine

--- src/app.js
+++ src/app.js
@@ -10,7 +10,6 @@ function showDate() {
     "Saturday",
   ];
   let day = days[now.getDay()];
-
   let hours = now.getHours();
   let minutes = now.getMinutes();
   if (minutes < 10) {
@@ -33,14 +32,33 @@ function displayTemperature(response) {
   let windElement = document.querySelector("#wind");
   let humidityElement = document.querySelector("#humidity");
   let feelsLikeElement = document.querySelector("#feelslike");
+  let iconElement = document.querySelector("#icon");
+
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
   feelsLikeElement.innerHTML = Math.round(response.data.temperature.feels_like);
   windElement.innerHTML = Math.round(response.data.wind.speed);
   humidityElement.innerHTML = response.data.temperature.humidity;
+  iconElement.setAttribute("src", response.data.condition.icon_url);
+  iconElement.setAttribute("alt", response.data.condition.icon);
 }
-let apiKey = "ee44ot6560132a4246a0947ebe4a550f";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=London&key=${apiKey}&units=metric`;
 
-axios.get(apiUrl).then(displayTemperature);
+function search(city) {
+  let apiKey = "ee44ot6560132a4246a0947ebe4a550f";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector("#city-input");
+  let city = cityInput.value;
+  search(city);
+}
+
+search("Kyiv");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
