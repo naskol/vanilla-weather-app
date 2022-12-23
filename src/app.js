8f7aedf4ca1ca588524c3efe4a5839715fc81e19
naskol <44285828+naskol@users.noreply.github.com>
2022-12-23 16:35:22 +0200

    Update the currant time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,30 @@
+function showDate() {
+  let now = new Date();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[now.getDay()];
+
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let currantTime = document.querySelector("#date");
+  currantTime.innerHTML = `${day} ${hours}:${minutes}`;
+}
+showDate();
+
 function displayTemperature(response) {
   console.log(response);
   let temperatureElement = document.querySelector("#temperature");
